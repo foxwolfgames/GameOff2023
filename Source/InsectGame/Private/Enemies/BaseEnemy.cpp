011703335d055a5e163ac9b3c69c13bc02d259f0
Coco <cocochen12301@gmail.com>
2023-11-13 16:43:20 -0800

    Revert "feat: Enemy manager class"
    This reverts commit 1564e03dd3685cc94a9566200b0202bd6590788d.

---
+++ Source/InsectGame/Private/Enemies/BaseEnemy.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemies/BaseEnemy.h"
+#include "BaseGameInstance.h"
+#include "Enemies/NavigationManager.h"
+//Components
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "AIController.h"
+
+// Sets default values
+ABaseEnemy::ABaseEnemy()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	GetMesh()->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block);
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->bUseRVOAvoidance = true;
+	GetCharacterMovement()->MaxWalkSpeed = 150.f;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	//GetMesh()->SetGenerateOverlapEvents(true);
+}
+
+// Called when the game starts or when spawned
+void ABaseEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	AIController = Cast<AAIController>(GetController());
+	if (UBaseGameInstance* GameInstance = Cast<UBaseGameInstance>(GetGameInstance()))
+	{
+		NavigationManager = GameInstance->GetNavigationManager();
+	}
+	if (NavigationManager)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Navigation Manager Set"));
+		CurrentTarget =NavigationManager->TargetPointArr1[0];
+	}
+	//AIController->MoveToLocation(FVector(0.f, 0.f, 110.f), 20.f, true, true, true);
+}
+
+bool ABaseEnemy::InTargetRange(AActor* Target, double Radius)
+{
+	const double DistanceToTarget = (Target->GetActorLocation() - GetActorLocation()).Size();
+	return DistanceToTarget <= Radius;
+}
+
+// Called every frame
+void ABaseEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (CurrentTarget && AIController)
+	{
+		if (!InTargetRange(CurrentTarget, 200.f))
+		{
+			AIController->MoveToActor(CurrentTarget);
+		}
+	}
+	
+}
+
+// Called to bind functionality to input
+void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/InsectGame/Public/Enemies/NavigationManager.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NavigationManager.generated.h"
+
+UCLASS()
+class INSECTGAME_API ANavigationManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ANavigationManager();
+	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, Category = "Navigation")
+	TArray<AActor*> TargetPointArr1;
+	UPROPERTY(EditAnywhere, Category = "Navigation")
+	TArray<AActor*> TargetPointArr2;
+	UPROPERTY(EditAnywhere, Category = "Navigation")
+	TArray<AActor*> TargetPointArr3;
+	UPROPERTY(EditAnywhere, Category = "Navigation")
+	TArray<AActor*> TargetPointArr4;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:	
+};
